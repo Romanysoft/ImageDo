f251388e2c0b6a0b25842a59762f86ad05b3cac3
iansun <lauer3912@gmail.com>
2016-12-05 12:28:31 +0800

    ImageDo update

---
+++ js/config.js
@@ -0,0 +1,15 @@
+(function () {
+    window.RTYConfig = {
+        appName:"ImageDo",
+        appID:"com.romanysoft.app.macos.pImageDo",
+        documentTitle:"ImageDo By Romanysoft",
+        supportPlatforms:["Mac"],
+        googleUA:"UA-87767476-3",
+        gitHome:"//github.com/Romanysoft/ImageDo",
+        reportIssueUrl:"//github.com/Romanysoft/ImageDo/issues",
+        changeLogUrl:"//github.com/Romanysoft/ImageDo/wiki/Changelog",
+        wikiUrl:"//github.com/Romanysoft/ImageDo/wiki",
+        romanysoftHome:"//www.romanysoft.com",
+        copyright:"Copyright © " + (new Date()).getFullYear() + " Romanysoft LAB."
+    }
+})();
